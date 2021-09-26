7c148a32f64961d573408eba0cd38f6963b1415f
NabarunKar <nabarunkar01@gmail.com>
2021-09-26 23:13:42 +0530

    refactoring

--- main.py
+++ main.py
@@ -8,39 +8,13 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-# x_position = [0, -20, -40]
-# segments = []
-
-
 snake = Snake()
 game_is_on = True
-# for turtle_index in range(0, 3):
-#     new_turtle = Turtle()
-#     new_turtle.penup()
-#     new_turtle.shape("square")
-#     new_turtle.color("white")
-#     segments.append(new_turtle)
-#     new_turtle.goto(x_position[turtle_index], 0)
+
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    # for seg_num in range(len(segments)-1, 0, -1):
-    #     new_x = segments[seg_num-1].xcor()
-    #     new_y = segments[seg_num-1].ycor()
-    #     segments[seg_num].goto(new_x, new_y)
-    # segments[0].forward(20)
-    # segments[0].left(90)
     snake.move()
-
-
-
-
-
-
-
-
-
-
 
 screen.exitonclick()
