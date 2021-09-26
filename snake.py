31404661e65f21e793d9730e356e66aac6c0275c
NabarunKar <nabarunkar01@gmail.com>
2021-09-27 01:45:16 +0530

    Collision with self

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 
-x_position = [0, -20, -40]
+# x_position = [0, -20, -40]
 
 move_distance = 20
 # game_is_on = True
@@ -8,7 +8,7 @@ UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
-
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 
 class Snake:
 
@@ -18,13 +18,19 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-        for turtle_index in range(0, 3):
-            new_turtle = Turtle()
-            new_turtle.penup()
-            new_turtle.shape("square")
-            new_turtle.color("white")
-            self.segments.append(new_turtle)
-            new_turtle.goto(x_position[turtle_index], 0)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_turtle = Turtle()
+        new_turtle.penup()
+        new_turtle.shape("square")
+        new_turtle.color("white")
+        self.segments.append(new_turtle)
+        new_turtle.goto(position)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
