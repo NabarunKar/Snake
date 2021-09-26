5ee7cbf024575cb9dc3510063f701ded0fd99a1e
NabarunKar <nabarunkar01@gmail.com>
2021-09-27 01:27:06 +0530

    Detecting collisions with walls

--- score.py
+++ score.py
@@ -12,6 +12,10 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f"Score = {self.score}", align="center", font=("Courier", 22, "normal"))
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=("Courier", 22, "normal"))
+
     def increase_score(self):
         self.score += 1
         self.clear()
